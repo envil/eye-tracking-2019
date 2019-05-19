3c1455adb5c5a605b9b6f7ce5a8c0e60999a0a2a
Viet Ta <viet.ta@bento.cloud>
2019-05-19 19:21:23 +0300

    Added results after running with 2 settings

--- project/task-1/task1.py
+++ project/task-1/task1.py
@@ -10,33 +10,47 @@ file_path = (base_path / '../data/train.csv').resolve()
 subjects = ['s7', 's17', 's27', 's3', 's13', 's23']
 
 
+def plot_fixations(index, info, data, fixation_centroids, setting):
+    plt.figure(dpi=150)
+    plt.plot(data[:, 0], data[:, 1], zorder=1)
+    plt.plot(fixation_centroids[:, 0], fixation_centroids[:, 1], c='r', linewidth=0.5, zorder=2)
+    plt.scatter(fixation_centroids[:, 0], fixation_centroids[:, 1], c='r', s=30, zorder=3)
+    plt.title('({}) Subject: {}, Known = {}, Degree = {}, Duration = {}'.format(index, info[0], info[1], setting.degree, setting.duration))
+    plt.savefig('./figures/setting2/{}.svg'.format(index))
+    # plt.show()
+
+
 def task1(setting):
     with file_path.open() as file:
         data = []
         test_info = []
+        results = []
+        i = 0
         for row in csv.reader(file, delimiter=','):
             if row[0] in subjects:
                 test_info.append((row[0], row[1] == 'true'))
                 row = np.array(row[2:])
                 row = row.astype(np.float)
                 data.append(np.reshape(row, (int(row.shape[0] / 2), 2)))
-
-    print(data[0].shape)
-
-    fixation_centroids, fixation_time = detect_fixations(data[0], setting.sampling_frequency, setting.get_window_size(),
+                fixation_centroids, fixation_time = detect_fixations(data[-1], setting.sampling_frequency, setting.get_window_size(),
                                                          setting.get_threshold())
+
+                plot_fixations(i, test_info[-1], data[-1], fixation_centroids, setting)
+                i += 1
+
+    # fixation_centroids, fixation_time = detect_fixations(data[0], setting.sampling_frequency,
+    #                                                      setting.get_window_size(),
+    #                                                      setting.get_threshold())
+    # plot_fixations(test_info[0], data[0], fixation_centroids, setting)
     # print(fixation_time)
     # print(fixation_centroids)
-    plt.plot(fixation_centroids[:, 0], fixation_centroids[:, 1], c='r')
-    plt.plot(data[0][:, 0], data[0][:, 1])
-    plt.show()
 
 
-setting1 = Setting(1, 0.03)
-setting2 = Setting(1, 0.1)
-setting3 = Setting(2, 0.03)
+# setting1 = Setting(1, 0.03)
+# task1(setting1)
+# setting2 = Setting(1, 0.1)
+# task1(setting2)
+# setting3 = Setting(2, 0.03)
+# task1(setting3)
 setting4 = Setting(2, 0.1)
-task1(setting1)
-task1(setting2)
-task1(setting3)
 task1(setting4)
